829dc269c316c0d0c3e881ec5d7e3a2f144da79f
Alexigbokwe <juvitafrica@gmail.com>
2023-10-27 18:56:30 +0100

    first commit

---
+++ src/main/java/com/pharmacy/management/pms/entities/Token.java
@@ -0,0 +1,5 @@
+package com.pharmacy.management.pms.entities;
+
+public class Token {
+
+}
---
+++ src/test/java/com/pharmacy/management/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharmacy.management.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
